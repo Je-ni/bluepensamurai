61cdf870b085aa0c968e8b75b7abb3cc6deb5722
Olibie Chidera <jenniferolibie@gmail.com>
2020-08-06 12:11:48 +0100

    initialize vue app

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ name }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
@@ -99,6 +99,11 @@ export default {
   name: "HelloWorld",
   props: {
     msg: String
+  },
+  data() {
+    return {
+      name: "jenny"
+    };
   }
 };
 </script>
