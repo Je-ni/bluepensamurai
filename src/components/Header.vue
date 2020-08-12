6349166723fa3cc4817d7fd8b872891cb32ed2f6
Olibie Chidera <jenniferolibie@gmail.com>
2020-08-12 17:55:54 +0100

    added header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,81 @@
 <template>
   <header>
-    <nav>
-      <ul>
-        <li><router-link to="/">Logo</router-link></li>
-        <li><router-link to="/">gallery</router-link></li>
-        <li><router-link to="/about">about</router-link></li>
-        <li><router-link to="/contact">contact me</router-link></li>
-      </ul>
+    <nav class="flex items-center justify-between flex-wrap px-6 py-6 md:px-16">
+      <div class="flex items-center flex-shrink-0 text-blue mr-6">
+        <svg
+          class="fill-current h-8 w-8 mr-2"
+          width="54"
+          height="54"
+          viewBox="0 0 54 54"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"
+          />
+        </svg>
+        <span class="font-semibold text-xl tracking-tight">
+          <router-link to="/">
+            Bluepen Samurai
+          </router-link>
+        </span>
+      </div>
+      <div class="block md:hidden">
+        <button
+          @click="isOpen = !isOpen"
+          class="flex items-center px-3 py-2 border rounded text-blue border-white hover:text-blue-400 hover:border-blue-400"
+        >
+          <svg
+            class="fill-current h-3 w-3"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+          </svg>
+        </button>
+      </div>
+      <div
+        :class="isOpen ? 'block' : 'hidden'"
+        class="w-full block flex-grow md:flex md:justify-end md:w-auto"
+      >
+        <div class="text-sm md:flex-grow md:flex md:justify-end">
+          <router-link
+            to="/"
+            class="block mt-4 md:inline-block md:mt-0 text-blue hover:text-blue-400 mr-4"
+          >
+            gallery
+          </router-link>
+          <router-link
+            to="/about"
+            class="block mt-4 md:inline-block md:mt-0 text-blue hover:text-blue-400 mr-4"
+          >
+            about
+          </router-link>
+          <router-link
+            to="/contact"
+            class="block mt-4 md:inline-block md:mt-0 text-blue hover:text-blue-400 mr-4"
+          >
+            contact
+          </router-link>
+        </div>
+      </div>
     </nav>
   </header>
 </template>
 
 <script>
 export default {
-  name: "Header"
+  name: "Header",
+  data() {
+    return {
+      isOpen: false
+    };
+  },
+  methods: {
+    navToggler: function() {
+      this.navShow = !this.navShow;
+    }
+  }
 };
 </script>
 
