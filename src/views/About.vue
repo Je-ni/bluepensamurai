7eb7542c36e2f1839df7516718fd2443556f0657
Olibie Chidera <jenniferolibie@gmail.com>
2020-08-15 21:26:21 +0100

    added about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="about">
+  <div class="grid md:grid-cols-2">
     <h1>This is an about page</h1>
+    <p>
+      i am a warrior that wanders the land and encounters moments. every moment
+      has lines and feelings in them which i have to illustrate. these are
+      battles; some are strong, some are weak, some are kind, some are dirty,
+      some are elusive, and some dance. but i have to defeat them so they can be
+      my friends. all those lines that bend a lot, all those impulses,
+      expressions, urges, conversations, flashes that go away when you blink,
+      all those incomplete pictures that come together to make one story, that’s
+      me, that’s my work, and these are my people.
+    </p>
+    <img src="./img/profile.jpg" alt="" />
   </div>
 </template>
