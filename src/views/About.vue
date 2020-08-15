8540511414f66c272d949e4f098103821c076e78
Olibie Chidera <jenniferolibie@gmail.com>
2020-08-16 00:37:51 +0100

    added contact to about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="grid md:grid-cols-2 gap-3 px-6 py-6 md:px-16">
-    <div>
-      <h1 class="font-normal border-b-2 text-2xl">chiemelie olibie</h1>
+  <div class="grid md:grid-cols-2 gap-5 px-6 py-3">
+    <img class="max-w-lg w-full" :src="profile" alt="Chiemelie Olibie" />
+    <div class="md:row-start-1">
+      <h1 class="font-normal border-b-2 border-current text-2xl">
+        chiemelie olibie
+      </h1>
       <p class="py-2 text-justify">
         i am a warrior that wanders the land and encounters moments. every
         moment has lines and feelings in them which i have to illustrate. these
@@ -11,13 +14,14 @@
         impulses, expressions, urges, conversations, flashes that go away when
         you blink, all those incomplete pictures that come together to make one
         story, that’s me, that’s my work, and these are my people.
+        <br />
+        <br />
+        <a href="mailto:email: geraldoc@gmail.com"
+          >Mail me:
+          <span class="hover:text-blue-400"> geraldoc@gmail.com </span></a
+        >
       </p>
     </div>
-    <img
-      class="max-w-lg sm:row-start-1"
-      :src="profile"
-      alt="Chiemelie Olibie"
-    />
   </div>
 </template>
 
