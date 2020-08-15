a64dff9ee1d3d9c3f91fa00a52e547859d784d6f
Olibie Chidera <jenniferolibie@gmail.com>
2020-08-15 21:51:03 +0100

    added about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,16 +1,35 @@
 <template>
-  <div class="grid md:grid-cols-2">
-    <h1>This is an about page</h1>
-    <p>
-      i am a warrior that wanders the land and encounters moments. every moment
-      has lines and feelings in them which i have to illustrate. these are
-      battles; some are strong, some are weak, some are kind, some are dirty,
-      some are elusive, and some dance. but i have to defeat them so they can be
-      my friends. all those lines that bend a lot, all those impulses,
-      expressions, urges, conversations, flashes that go away when you blink,
-      all those incomplete pictures that come together to make one story, that’s
-      me, that’s my work, and these are my people.
-    </p>
-    <img src="./img/profile.jpg" alt="" />
+  <div class="grid md:grid-cols-2 gap-5 px-6 py-6 md:px-16">
+    <div>
+      <h1 class="font-normal border-b-2 text-2xl">chiemelie olibie</h1>
+      <p class="py-2 text-justify">
+        i am a warrior that wanders the land and encounters moments. every
+        moment has lines and feelings in them which i have to illustrate. these
+        are battles; some are strong, some are weak, some are kind, some are
+        dirty, some are elusive, and some dance. but i have to defeat them so
+        they can be my friends. all those lines that bend a lot, all those
+        impulses, expressions, urges, conversations, flashes that go away when
+        you blink, all those incomplete pictures that come together to make one
+        story, that’s me, that’s my work, and these are my people.
+      </p>
+    </div>
+    <img
+      class="max-w-lg sm:row-start-1"
+      :src="profile"
+      alt="Chiemelie Olibie"
+    />
   </div>
 </template>
+
+<script>
+import profile from "@/assets/img/profile.jpg";
+
+export default {
+  name: "About",
+  data() {
+    return {
+      profile: profile
+    };
+  }
+};
+</script>
